d154da9d464196afdea3dcd31d7fd2d46cdd1a61
hadonga <hadonga@gmail.com>
2024-01-05 11:37:13 +0900

    test_ipm

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # horizon_oe
+## For more details, please find in https://sapeon.atlassian.net/wiki/spaces/~6413f81a57f0c028e2f53406/pages/edit-v2/284229893
--- oe_v1_1_68/run_docker.sh
+++ oe_v1_1_68/run_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#usage bash run_docker.sh /data gpu
+#usage: 
+#  $bash run_docker.sh /data gpu
 
 dataset_path=$1
 run_type=$2
-version=v1.1.62
+version=v1.1.68
 
 if [ -z "$dataset_path" ];then
   echo "Please specify the dataset path"
@@ -11,9 +12,8 @@ if [ -z "$dataset_path" ];then
 fi
 dataset_path=$(readlink -f "$dataset_path")
 
-echo "Docker version is ${version}"
-echo "Dataset path is $(readlink -f "$dataset_path")"
-
+echo "Setting up Docker for OpenExplorer ${version}"
+echo "Host dataset path is $(readlink -f "$dataset_path")"
 open_explorer_path=$(readlink -f "$(dirname "$0")")
 echo "OpenExplorer package path is $open_explorer_path"
 
@@ -21,15 +21,15 @@ if [ "$run_type" == "cpu" ];then
     echo "Start Docker container in CPU mode."
     docker run -it --rm \
       -v "$open_explorer_path":/open_explorer \
-      -v "$dataset_path":/data/horizon_j5/data \
+      -v "$dataset_path":/data \
       openexplorer/ai_toolchain_ubuntu_20_j5_cpu:"$version"
 else
     echo "Start Docker container in GPU mode."
     docker run -it --rm \
       --gpus all \
       --privileged \
-      --shm-size="15g" \
+      --shm-size="32g" \
       -v "$open_explorer_path":/open_explorer \
-      -v "$dataset_path":/data/horizon_j5/data \
+      -v "$dataset_path":/data \
       openexplorer/ai_toolchain_ubuntu_20_j5_gpu:"$version"
 fi
